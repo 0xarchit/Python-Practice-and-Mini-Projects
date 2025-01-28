44c6ea146c4426b8e0c6e4bb119815b4d2a8ca86
Archit Jain <0xarchit@gmail.com>
2025-01-28 23:56:02 +0530

    snake game progress

--- Self/Turtle/snake game/main.py
+++ Self/Turtle/snake game/main.py
@@ -11,13 +11,27 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = snake.Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+
+
+
+
+
+
+
+
 
 
 
