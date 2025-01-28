95b39042a97738ebd5cfed536e8fb06bcdd74d80
Archit Jain <0xarchit@gmail.com>
2025-01-28 06:23:40 +0530

    bug fix

--- Self/Turtle/snake game/main.py
+++ Self/Turtle/snake game/main.py
@@ -2,6 +2,7 @@ import turtle as t
 import random
 from tkinter import messagebox
 import time
+import snake
 
 screen = t.Screen()
 screen.setup(width=600, height=600)
@@ -9,28 +10,14 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-segments = []
-
-start_pos = [(0,0), (-20,0), (-40,0)]
-for pos in start_pos:
-    new_segment = t.Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(pos)
-    segments.append(new_segment)
-
-
+snake = snake.Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.5)
-    
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    time.sleep(0.1)
+
+    snake.move()
 
 
 
